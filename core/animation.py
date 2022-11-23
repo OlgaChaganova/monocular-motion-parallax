4365638596ba8970927440be4843a011d132b118
oliyyaa <olga.chaganova1@gmail.com>
2022-11-23 22:34:46 +0300

    Update

--- core/animation.py
+++ core/animation.py
@@ -28,27 +28,28 @@ class DynamicAnimation(object):
         return xx, yy, zz, config['color']
 
     def make_animation(self):
-        xx1, yy1, zz1, color1 = self._generate_points(self._config_cyl['cylinder_1'])
-        xx2, yy2, zz2, color2 = self._generate_points(self._config_cyl['cylinder_2'])
-
-        print(xx1.shape, xx2.shape)
-
-        # Create a figure and a 3D Axes
-        fig = plt.figure()
-        ax = Axes3D(fig)
-        ax.axis('off')
-
         def init():
-            ax.scatter(xx1, yy1, zz1, marker='o', s=20, c=color1, alpha=0.6)
-            ax.scatter(xx2, yy2, zz2, marker='o', s=20, c=color2, alpha=0.6)
+            ax.scatter(xx1, yy1, zz1, marker='o', s=20, c=color1, alpha=0.7)
+            ax.scatter(xx2, yy2, zz2, marker='o', s=20, c=color2, alpha=0.7)
             return fig,
 
         def animate(i):
-            ax.view_init(elev=0., azim=i)
+            ax.view_init(azim=(dest_coeff * i))
+            ax.dist = self._config_anim['dist']
             return fig,
 
+        xx1, yy1, zz1, color1 = self._generate_points(self._config_cyl['cylinder_1'])
+        xx2, yy2, zz2, color2 = self._generate_points(self._config_cyl['cylinder_2'])
+        dest_coeff = 1 if self._config_anim['direction'] == 'counterclockwise' else -1  # TODO: not working as expected
+
+        # Create a figure and a 3D Axes
+        fig = plt.figure(figsize=(7, 7))
+        ax = Axes3D(fig, elev=self._config_anim['elev'])
+        ax.dist = self._config_anim['dist']
+        ax.axis('off')
+
         anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                       frames=360, interval=10, blit=True)
+                                       frames=360, interval=1, blit=True)
 
         anim.save(self._config_anim['save_path'], fps=self._config_anim['fps'])
 
---
+++ core/main.py
@@ -0,0 +1,6 @@
+from animation import DynamicAnimation
+
+
+if __name__ == '__main__':
+    dynamic_animation = DynamicAnimation()
+    dynamic_animation.make_animation()
