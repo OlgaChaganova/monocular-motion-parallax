Fix rotation

core/animation.py
@@ -1,4 +1,5 @@
 import typing as tp
+from time import time
 
 import numpy as np
 from omegaconf import OmegaConf
@@ -10,21 +11,23 @@ from cylinder import Cylinder
 
 
 class DynamicAnimation(object):
-    def __init__(self):
-        self._config = OmegaConf.load('../config.yml')
+    def __init__(self, config_path: str = '../config.yml'):
+        self._config = OmegaConf.load(config_path)
         self._config_cyl = self._config['cylinders']
         self._config_anim = self._config['animation']
 
+        self.cylinder1 = Cylinder(rho=self._config_cyl['cylinder_1']['rho'], z=self._config_cyl['cylinder_1']['z'])
+        self.cylinder1.generate_points(points_cnt=self._config_cyl['cylinder_1']['points_cnt'])
+
+        self.cylinder2 = Cylinder(rho=self._config_cyl['cylinder_2']['rho'], z=self._config_cyl['cylinder_2']['z'])
+        self.cylinder2.generate_points(points_cnt=self._config_cyl['cylinder_2']['points_cnt'])
+
     @classmethod
-    def _generate_points(cls, config: dict) -> tp.Tuple[np.array, np.array, np.array, str]:
-        cylinder = Cylinder(
-            rho=config['rho'],
-            z=config['z'],
-        )
-        cylinder.generate_random_points(points_cnt=config['points_cnt'])
-        xx = cylinder.points[:, 0]
-        yy = cylinder.points[:, 1]
-        zz = cylinder.points[:, 2]
+    def _get_points(cls, cylinder: Cylinder, config: dict) -> tp.Tuple[np.array, np.array, np.array, str]:
+        cartesian_coords = cylinder.cylindrical2cartesian(cylinder.points)
+        xx = cartesian_coords[:, 0]
+        yy = cartesian_coords[:, 1]
+        zz = cartesian_coords[:, 2]
         return xx, yy, zz, config['color']
 
     def make_animation(self):
@@ -34,26 +37,41 @@ class DynamicAnimation(object):
             return fig,
 
         def animate(i):
-            ax.view_init(azim=(dest_coeff * i))
+            start_time = time()
+            ax = Axes3D(fig, elev=self._config_anim['elev'])
             ax.dist = self._config_anim['dist']
+            ax.axis('off')
+            self.cylinder1.rotate(direction='counterclockwise', delta_phi=self._config_anim['delta_phi'])
+            self.cylinder2.rotate(direction='clockwise', delta_phi=self._config_anim['delta_phi'])
+            xx1, yy1, zz1, color1 = self._get_points(self.cylinder1, self._config_cyl['cylinder_1'])
+            xx2, yy2, zz2, color2 = self._get_points(self.cylinder2, self._config_cyl['cylinder_2'])
+            ax.scatter(xx1, yy1, zz1, marker='o', s=20, c=color1, alpha=0.7)
+            ax.scatter(xx2, yy2, zz2, marker='o', s=20, c=color2, alpha=0.7)
+            # print(f'# {i} time: {time() - start_time}')
             return fig,
-
-        xx1, yy1, zz1, color1 = self._generate_points(self._config_cyl['cylinder_1'])
-        xx2, yy2, zz2, color2 = self._generate_points(self._config_cyl['cylinder_2'])
-        dest_coeff = 1 if self._config_anim['direction'] == 'counterclockwise' else -1  # TODO: not working as expected
 
         # Create a figure and a 3D Axes
         fig = plt.figure(figsize=(7, 7))
         ax = Axes3D(fig, elev=self._config_anim['elev'])
         ax.dist = self._config_anim['dist']
         ax.axis('off')
+        xx1, yy1, zz1, color1 = self._get_points(self.cylinder1, self._config_cyl['cylinder_1'])
+        xx2, yy2, zz2, color2 = self._get_points(self.cylinder2, self._config_cyl['cylinder_2'])
 
-        anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                       frames=360, interval=1, blit=True)
-
+        anim = animation.FuncAnimation(
+            fig,
+            animate,
+            init_func=init,
+            frames=(360 // self._config_anim['delta_phi']),
+            interval=1,
+            blit=True,
+        )
         anim.save(self._config_anim['save_path'], fps=self._config_anim['fps'])
 
 
 if __name__ == '__main__':
     dynamic_animation = DynamicAnimation()
+    start_time = time()
     dynamic_animation.make_animation()
+    end_time = time()
+    print(f'Generation time: {end_time - start_time}')

core/cylinder.py
@@ -10,7 +10,7 @@ class Cylinder(object):
         self._z = z
         self.points = np.array([])
 
-    def generate_random_points(self, points_cnt: int) -> None:
+    def generate_points(self, points_cnt: int) -> None:
         """
         Generate random points.
 
@@ -24,12 +24,40 @@ class Cylinder(object):
         np.array
             Array with (x, y, z) coordinates placed at the row. Shape: (points_cnt, 3).
         """
-
         phis = np.random.uniform(low=0, high=(2 * np.pi), size=points_cnt)
         zs = np.random.uniform(low=self._z[0], high=self._z[1], size=points_cnt)
+        self.points = np.column_stack([phis, zs])
+
+    def cylindrical2cartesian(self, points: np.array) -> np.array:
+        """
+        Move from cylindrical (rho, phi, z) to cartesian (x, y, z) coordinate system.
+
+        Parameters
+        ----------
+        points : np.array
+            Points of the cylinder in cylindrical coordinate system (phi, z). Shape: [points_cnt, 2].
+
+        Returns
+        -------
+        np.array
+            Points in cartesian (x, y, z) coordinate system. Shape: [points_cnt, 3]
+        """
+        phis, zs = points[:, 0], points[:, 1]
         xs = self._rho * np.cos(phis)
         ys = self._rho * np.sin(phis)
-        self.points = np.column_stack([xs, ys, zs])
+        return np.column_stack([xs, ys, zs])
+
+    def rotate(
+        self,
+        direction: tp.Literal['clockwise', 'counterclockwise'],
+        delta_phi: float = 1,
+    ):
+        assert direction in ['clockwise', 'counterclockwise'], \
+            'Direction must be one of the following: clockwise, counterclockwise'
+        if direction == 'clockwise':
+            self.points[:, 0] -= delta_phi * np.pi / 180
+        else:
+            self.points[:, 0] += delta_phi * np.pi / 180
 
 
 if __name__ == '__main__':